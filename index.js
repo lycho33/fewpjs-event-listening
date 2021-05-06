22e82947c5e6b0db68db6add12bf3e26c734e07b
lycho33 <cho.lyds@gmail.com>
2021-05-06 18:58:07 -0400

    commit tree aaad5f1085e57eb93fbe0b01afd45b74e46f20d5

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
-}
+    input.addEventListener('click', function(event) {
+      return "clicked"
+    });
+  }
+
+//Added an event listern so it listens for the 'click' event -> execute the callback function
